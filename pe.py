f36284fbb66259c76202b327e4915a273c5979b2
liebesu <lichunjiang42@gmail.com>
2016-04-13 18:24:03 +0800

    update
    update

--- pe.py
+++ pe.py
@@ -24,6 +24,13 @@ def get_pe_sha256(PATH):
     a=open("sha256","a")
     a.write(sha256+"\n")
     a.close()
+def get_pe_md5(PATH):
+    with open(PATH,'rb') as f:
+        md5obj = hashlib.md5()
+        md5obj.update(f.read())
+        hash = md5obj.hexdigest()
+        print(hash)
+        return hash
 def get_path(ROOTPATH):
     pefiles=os.path.join(ROOTPATH,"pefiles")
     for root ,dirs,files in os.walk(pefiles):

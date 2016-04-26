8fbc272ea485b4693d43401ff35ff2d069372612
liebesu <lichunjiang42@gmail.com>
2016-04-26 18:37:04 +0800

    update
    update

--- pe.py
+++ pe.py
@@ -1,6 +1,8 @@
 import hashlib
 from multiprocessing import Pool
 import os
+import MySQLdb
+import shutil
 
 __author__ = 'liebesu'
 
@@ -15,7 +17,7 @@ def get_pe_imports(PATH):
         for imp in entry.imports:
             print "\t",imp.name
 
-def get_pe_sha256(PATH):
+def get_pe_sha256(PATH,sha256s):
 
     f = open(PATH, "rb")
     BUFSIZE = 1024*1024
@@ -24,10 +26,28 @@ def get_pe_sha256(PATH):
     print sha256
     #get_pe_imports(PATH)
 
-    a=open("sha256","a")
+    '''a=open("sha256","a")
     a.write(sha256+"\n" )
     a.write(PATH+"\n")
-    a.close()
+    a.close()'''
+    if sha256 in sha256s:
+        shutil.copy(PATH,"white_exe")
+def dabase_select():
+    db= MySQLdb.connect(db="malware_info", user="root", passwd="polydata", host="localhost", port=3306,charset='utf8')
+
+    cursor = db.cursor()
+    try:
+        select_sql="select sha256 from lvmeng_dll_exe_5m_white"
+        cursor.execute(select_sql)
+        sha256s=cursor.fetchall()
+        sha256s_list=[str(x).replace('u',"").replace("(","").replace(")","").replace(",","").replace("'","")for x in list(sha256s)]
+        db.commit()
+        cursor.close()
+        db.close()
+        return sha256s_list
+    except Exception as e:
+        print e
+        exit()
 def get_pe_md5(PATH):
     with open(PATH,'rb') as f:
         md5obj = hashlib.md5()
@@ -36,14 +56,14 @@ def get_pe_md5(PATH):
         a=open("md5","a")
         a.write(hash+"\n")
         a.close()
-def get_path(ROOTPATH):
-    pefiles=os.path.join(ROOTPATH,"pefiles")
+def get_path(ROOTPATH,sha256s):
+    pefiles=os.path.join(ROOTPATH,"lvmeng_dll_5m")
     for root ,dirs,files in os.walk(pefiles):
         for file in files:
             filepa=os.path.join(root,file)
             print filepa
             try:
-                get_pe_sha256(filepa)
+                get_pe_sha256(filepa,sha256s)
                 #get_pe_md5(filepa)
             except Exception as e:
                 print e
@@ -51,6 +71,9 @@ def get_path(ROOTPATH):
 
 
 if __name__=="__main__":
-   get_path(ROOTPATH)
+    sha256s=dabase_select()
+    print sha256s
+    print type(sha256s)
+    get_path(ROOTPATH,sha256s)
 
 
--- test_numpy.py
+++ test_numpy.py
@@ -2,29 +2,116 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-a=open('json/10.txt',"r").read().replace('"',"'")
-print type(a)
-a=eval(a)
-print type(a)
-print a
-dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
-print dict['Name']
-print a['1']
-'''
-x = np.linspace(0, 10, 1000)
-y = np.cos(x)
-z = np.cos(x**2)
+from mpl_toolkits.mplot3d import Axes3D
 
-plt.figure(figsize=(9,4))
-plt.plot(x,y,label="$sin(x)$",color="red",linewidth=2)
-plt.plot(x,z,"b--",label="$cos(x^2)$")
-plt.xlabel("Time(s)")
-plt.ylabel("Volt")
-plt.title("PyPlot First Example")
-plt.ylim(-1.2,1.2)
-plt.legend()
-plt.show()
-'''
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')    #为y轴加注释
-plt.show()
+
+def open_get(path):
+    a=open(path,'r').read()
+    a=eval(a)
+    trim(a)
+def trim(a):
+    ids=[]
+    for id in a:
+
+        for whiteiteme in a[id]['white'].keys():
+            if "-" in whiteiteme:
+                del a[id]['white'][whiteiteme]
+        for wormiteme in a[id]['worm'].keys():
+            if "-" in wormiteme:
+                del a[id]['worm'][wormiteme]
+        inter_keys=list(set(a[id]['worm'].keys()).intersection(set(a[id]['white'].keys())))
+
+        #print inter_keys
+        white_percent_list=[]
+        worm_percent_list=[]
+        square_percent_list=[]
+        nums=[]
+        for sig_inter_key in inter_keys:
+
+            all_white_valus=0
+            all_worm_valus=0
+            white_values = 0
+            worm_values =0
+            for whiteiteme in a[id]['white'].keys():
+                all_white_valus +=float(a[id]['white'][whiteiteme])
+            for whiteiteme in a[id]['white'].keys():
+                if int(sig_inter_key)>=int(whiteiteme):
+                    white_valu=a[id]['white'][whiteiteme]
+                    white_values +=int(white_valu)
+                    white_percent=white_values/all_white_valus
+            #white_percent_list.append(white_percent)
+
+            for wormiteme in a[id]['worm'].keys():
+                all_worm_valus +=float(a[id]['worm'][wormiteme])
+            for wormiteme in a[id]['worm'].keys():
+                if int(sig_inter_key)>=int(wormiteme):
+                    worm_valu=a[id]['worm'][wormiteme]
+                    worm_values +=int(worm_valu)
+                    worm_percent=1-(worm_values/all_worm_valus)
+
+
+            ids.append(id)
+            nums.append(sig_inter_key)
+            square=(worm_percent*worm_percent)+(white_percent*white_percent)
+            square_percent_list.append(square)
+
+            #worm_percent_list.append(worm_percent)
+            #white_percent=1-white_percent
+            #if np.sqrt((worm_percent*worm_percent)-(white_percent*white_percent))>0.623:
+            '''if white_percent+worm_percent>1.2:
+                white_percent=1-white_percent
+                if np.sqrt((worm_percent*worm_percent)-(white_percent*white_percent))>0.50:
+                    print "id:",id
+                    print "num:",sig_inter_key
+                    print "all_white_valus",all_white_valus
+                    print "all_worm_valus",all_worm_valus
+                    print "white_percent:",white_percent
+                    print "worm_percent:",worm_percent
+                    white_percent_list.append(white_percent)
+                    worm_percent_list.append(worm_percent)
+
+            print sig_inter_key
+            print white_percent
+            print worm_percent
+            print white_percent_list,worm_percent_list'''
+        '''plt.plot(white_percent_list,worm_percent_list,'ro')
+        plt.title("PE Detection Rate")
+        plt.xlabel('White_percent')
+        plt.ylabel('Malware_percent')    #为y轴加注释
+        plt.plot(white_percent_list,worm_percent_list,'ro')
+        plt.title("PE Detection Rate")
+        plt.xlabel('White_percent')
+        plt.ylabel('Malware_percent')    #为y轴加注释'''
+    print ids,nums,square_percent_list
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_surface(ids, nums, square_percent_list, rstride=1, cstride=1,cmap='rainbow')
+    plt.show()
+    #print max(white_percent_list)
+    #print max(worm_percent_list)
+    #plt.show()
+
+
+def pre_pares(a):
+    pass
+
+
+def get_dec(a):
+    for id in a:
+        print id
+        feature_num=a[id]['feature_num']
+        api_ratio=a[id]['api_ratio']
+        white=a[id]['white']
+        worm=a[id]['worm']
+        api_thresh=a[id]['api_thresh']
+        print feature_num,api_ratio,worm.keys(),worm.values(),worm,api_thresh
+
+
+
+        plt.plot(worm.keys(),worm.values())
+        plt.ylabel('worm')    #为y轴加注释
+        plt.show()
+
+if __name__ == "__main__" :
+    result_path="json/31.txt"
+    open_get(result_path)

78b1f93031b91cdfb7a6193a2afbd2afbac876a4
liebesu <lichunjiang42@gmail.com>
2016-05-30 18:49:31 +0800

    add watcherlab_feed
    add watcherlab_feed

--- watcherlab_feed.py
+++ watcherlab_feed.py
@@ -1,8 +1,10 @@
 import os
 import re
+import tarfile
 import urllib
 
 import requests
+import sys
 from bs4 import BeautifulSoup
 import urlparse
 
@@ -16,5 +18,21 @@ for tgz in tgzs:
         pass
     else:
         os.makedirs("watcherlab")
-    urllib.urlretrieve(tgzurl,os.path.join("watcherlab",os.path.basename(tgzurl)))
+    if os.path.exists(os.path.join("watcherlab",os.path.basename(tgzurl))):
+        pass
+    else:
+        tgz_path=os.path.join("watcherlab", os.path.basename(tgzurl))
+        urllib.urlretrieve(tgzurl,tgz_path)
+        if os.path.exists("watcherlab_untaz"):
+            pass
+        else:
+            os.makedirs("watcherlab_untaz")
+        os.system("tar zxvf "+tgz_path+" -C watcherlab_untaz")
+os.system('find . -name "watcherlab-email*" | xargs -i cut -f 2 -d "," {}  >watcherlab_blackemail.txt')
+os.system('find . -name "watcherlab-fqdn*" | xargs -i cut -f 2 -d "," {}  >watcherlab_blackdomain.txt')
+os.system('find . -name "watcherlab-ipv4*" | xargs -i cut -f 2 -d "," {}  >watcherlab_blackipv4.txt')
+os.system('find . -name "watcherlab-ipv6*" | xargs -i cut -f 2 -d "," {}  >watcherlab_blackipv6.txt')
+os.system('find . -name "watcherlab-md5*" | xargs -i cut -f 2 -d "," {}  >watcherlab_blackmd5.txt')
+os.system('find . -name "watcherlab-url*" | xargs -i cut -f 2 -d "," {}  >watcherlab_blackurl.txt')
+
 
